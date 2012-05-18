d1af8c1926f9641faa2b7c6a8250b2e495fc5d13
taguchi.2240 <taguchi.2240@gmail.com>
2012-05-18 17:16:19 +0900

    canvas sample

--- shareboard.js
+++ shareboard.js
@@ -1,15 +1,44 @@
 if (Meteor.is_client) {
-  Template.hello.greeting = function () {
-    return "Welcome to shareboard.";
-  };
 
-  Template.hello.events = {
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
-    }
-  };
+  var Draw = false;  
+  
+  Template.canvas.events = {
+
+   	'mousemove canvas' : function () {
+		console.log('canvas_mousemove'+Draw+event.pageX+":"+event.pageY);
+		//todo loadの書き方がわからない
+		if (this.load !== true) {
+			console.log('canvas_new');			
+			this.canvas = document.getElementById("canvas");
+			this.canvas.width = 800;
+			this.canvas.height = 600;
+			this.context = canvas.getContext('2d');
+			this.rect = canvas.getBoundingClientRect();			
+			this.load = true;
+		}
+									
+		this.endX = event.pageX - this.rect.left;
+		this.endY = event.pageY - this.rect.top;
+		
+		if(Draw){
+			this.context.beginPath();
+			this.context.moveTo(this.startX, this.startY );
+			this.context.lineTo(this.endX, this.endY);
+			this.context.stroke();
+			this.context.save();
+		}
+		this.startX = this.endX;
+		this.startY = this.endY; 
+	  },
+      'mouseup canvas' : function () {
+          console.log("canvas mouseup");
+	      Draw=false;
+	  },
+      'mousedown canvas' : function () {
+          console.log("canvas mousedown");
+		  Draw=true;
+	  },
+  } ;
 }
 
 if (Meteor.is_server) {
